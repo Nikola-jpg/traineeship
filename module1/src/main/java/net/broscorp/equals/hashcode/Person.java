510545c67ffd7408b8b320e3a519bf91567ecee1
mbondarchuk <mbondarchuk@unitedthinkers.net>
2022-03-22 16:33:06 +0200

    Added javaDoc in Person.java

--- module1/src/main/java/net/broscorp/equals/hashcode/Person.java
+++ module1/src/main/java/net/broscorp/equals/hashcode/Person.java
@@ -8,36 +8,12 @@ public class Person {
   private int age;
 
   /**.
-   * Person constructor.
+   * Default constructor.
    */
   public Person(String firstName, String lastName, int age) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
   }
 
   @Override
@@ -53,6 +29,9 @@ public class Person {
         && Objects.equals(lastName, person.lastName);
   }
 
+  /**
+   * Изменил реализацию, так чтобы не вызывать другие библиотеки.
+   */
   @Override
   public int hashCode() {
     return firstName.hashCode() + lastName.hashCode() + age * 31;
